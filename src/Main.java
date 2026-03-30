3e2e41ab2aeca2027799ccc555fc2a27e36392bb
SuwireDaikon <ppm2004maksim@gmail.com>
2026-03-30 20:15:54 +0300

    Update Main.java
    Some format problems

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,7 @@ public class Main {
         byte bottleMachineProduced2Min = 16;
         int bottleMachineProduced = bottleMachineProduced2Min / 2;
         byte hour = 60; //minutes
-        int hour24 = hour*24;
+        int hour24 = hour * 24;
         bottleMachineProduced20min = bottleMachineProduced * 20;
         bottleMachineProducedHour24 = bottleMachineProduced * hour24;
         bottleMachineProduced3Hour24 = bottleMachineProduced * hour24 * 3;
@@ -74,7 +74,7 @@ public class Main {
         byte iceCreamQuantity = 2;
         byte rawEggsQuantity = 4;
         //recipeGrams
-        float recipeCocktailWeightG = (banana * bananaQuantity) + (milk*milkRQ)/100 + (iceCream * iceCreamQuantity) + (rawEggs * rawEggsQuantity);
+        float recipeCocktailWeightG = (banana * bananaQuantity) + (milk * milkRQ)/100 + (iceCream * iceCreamQuantity) + (rawEggs * rawEggsQuantity);
         float recipeCocktailWeightKg = recipeCocktailWeightG / 1000;
         System.out.println("Result in grams: " + recipeCocktailWeightG + ", result in kg: " + recipeCocktailWeightKg);
 
