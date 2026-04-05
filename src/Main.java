88a2a45359e7a33169f0f91b88277e86f92564f3
SiniWire <ppm2004maksim@gmail.com>
2026-04-05 20:46:23 +0300

    Rollin', checkin', doin' ##### Please, check the fixed homework.

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
             System.out.println("Установите lite-версию приложения для Android по ссылке");
         } else if (clientOS != 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS != 1 && clientDeviceYear < 2015) { // or clientOS == 0
+        } else if (clientOS != 1 && clientDeviceYear < 2015) {
             System.out.println("Установите lite-версию приложения для iOS по ссылке");
         }
 
