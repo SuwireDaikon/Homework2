51ea3cb01255810e278dd9a550d0608e08598ae1
SiniWire <ppm2004maksim@gmail.com>
2026-04-20 14:44:15 +0300

    Rollin', checkin', doin' ##### Please, check the homework.

---
+++ src/Author.java
@@ -0,0 +1,21 @@
+public class Author {
+    private String firstName;
+    private String surname;
+
+    public Author(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String toString() {
+        return firstName + " " + surname;
+    }
+}
